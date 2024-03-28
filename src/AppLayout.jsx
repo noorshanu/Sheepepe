eeea74c8bfdf8205857620f43f015fba48f46469
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-28 16:26:29 +0530

    nngg

---
+++ src/AppLayout.jsx
@@ -0,0 +1,99 @@
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import React, { useState, useRef } from "react";
+import Intro from "./sections/Intro";
+import MemeMark from "./sections/MemeMark";
+
+import Footer from "./sections/Footer";
+import Music from "./nubshiba.mp3";
+import Mission from "./sections/Mission";
+
+const Popup = ({ onClose }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center flex-col z-50  bg-white bg-hero">
+      <img src="images/lambo.jpeg" alt=""  className="h-[400px]"/>
+      <div className="rounded-lg p-8">
+        <button
+          className="bg-[#7cd259] rounded-full px-6 py-2 text--2xl sm:text-5xl font-short  font-bold border-2 hover:scale-125"
+          onClick={onClose}
+        >
+         Enter SheepPEPE Village
+        </button>
+      </div>
+    </div>
+  );
+};
+
+function AppLayout() {
+  const [showPopup, setShowPopup] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    // Start playing music here
+    const audio = audioRef.current;
+    audio.play();
+    setIsPlaying(true);
+  };
+
+  const handlePlayPause = () => {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <>
+      {showPopup && <Popup onClose={handleClosePopup} />}
+      <main className=" bg-main  h-[100vh] ">
+        {/* <Navbar /> */}
+
+        <Hero />
+        <div className=" flex justify-center my-4 ">
+          <button
+            className="bg-[#7cd259] text-black font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline  hover:scale-125"
+            onClick={handlePlayPause}
+          >
+            {isPlaying ? "Pause " : "Play Music"}
+          </button>
+        
+        </div>
+       
+        <audio ref={audioRef} src={Music}></audio>
+
+      
+      </main>
+
+      {/* <MemeMark /> */}
+
+      {/* <div>
+        <dl className="h-[50px]" />
+        <Intro />
+        <dl className="h-[50px]" />
+
+        <Mission/> */}
+        {/* <div>
+          <Photo/>
+        </div> */}
+        {/* <div className=" bg-[#ffffff]">
+        <MemeCards />
+        </div> */}
+        {/* <div>
+          <RoadMap/>
+        </div> */}
+        {/* <div>
+          <Partner />
+        </div> */}
+      {/* </div> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default AppLayout;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,57 @@
+import { BsTwitterX, BsInstagram, BsFacebook } from "react-icons/bs";
+import { FaLinkedinIn, FaTelegram } from "react-icons/fa6";
+
+function Hero() {
+  return (
+    <section className=" pt-0  pb-[1rem]">
+      <div className="container-wrapper flex justify-end flex-col items-center pt-[6rem] ">
+        <div></div>
+        <div className=" max-w-full sm:max-w-3xl ml-unset sm:ml-auto flex flex-col items-center  bg-[#7bd259ae] rounded-3xl border-2 ">
+          <img src="images/logo.png" alt="" className=" h-[250px]" />
+
+          <div>
+            <h1 className=" font-kid text-center text-3xl ">WELCOME TO THE <br />FLOCK BAAAAAAAA</h1>
+
+            <div className=" flex justify-center gap-5 items-center mt-[2rem]">
+              <a
+                href="https://twitter.com/NubshibSolana"
+                className=" text-xl text-black bg-[#7cd259] hover:scale-125 hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
+              >
+                <BsTwitterX />
+              </a>
+
+              <a
+                href="https://t.me/NubShib"
+                target="_blank"
+                className=" text-xl text-black hover:scale-125 bg-[#7cd259] hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
+              >
+                <FaTelegram />
+              </a>
+
+              <a
+                href="#"
+                target="_blank"
+                className=" bg-[#7cd259] rounded-full text-xl font-semibold p-1 border-2 hover:scale-125 "
+              >
+                <img
+                  src="images/dext.png"
+                  alt=""
+                  className=" h-[29px] w-auto"
+                />
+              </a>
+            </div>
+
+            <a
+              href="/"
+              className=" my-2 text-black font-bold py-2 px-2 sm:px-16 rounded sm:text-base text-xs font-short   flex justify-center w-[360px] sm:w-[580px] mx-auto"
+            >
+              BpAsimH7aFv2KFLDQeqWQkkCQh2to3ucfoaJ6UYfiDuk
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
